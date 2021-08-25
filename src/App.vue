2d2c4a7385b809179471c38485b7493f93a66879
Diana Dikovski <dianadi09@gmail.com>
2021-08-25 17:03:18 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app"
+       class="app">
+    <navigation/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/navigation.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navigation
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/css/_variables.scss";
+@import "@/assets/css/_reset.scss";
+@import "@/assets/css/_main.scss";
+</style>
